8429477a711abd3957ef7cee1af9dd6a9c36348e
Scott Bailey <rscottb31@gmail.com>
2019-12-03 16:42:21 -0500

    Added VS settings and resource files

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Sudoku")> 
+<Assembly: AssemblyDescription("Play the wonderful game of Sudoku!")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Sudoku")> 
+<Assembly: AssemblyCopyright("Copyright ©  2010-2012 Scott Bailey. All Rights Reserved.")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7c087098-ffe1-41ef-be1a-6257b80c5ceb")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
